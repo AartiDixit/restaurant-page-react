0f323feef460bb9acbeee9ac25dccca144b86256
AartiDixit <115448333+AartiDixit@users.noreply.github.com>
2023-02-13 08:21:44 -0800

    Add files via upload

---
+++ src/Components/Contact.js
@@ -0,0 +1,41 @@
+import React from "react";
+import contactimg from '../assets/images/contact.png'
+const Contact = () => {
+  return (
+    <>
+      <section class="contact" id="contact">
+        <h1 class="heading">
+          <span>contact</span> us
+        </h1>
+        <div class="row">
+          {/* <iframe
+            class="map"
+            src=""
+            allowfullscreen=""
+            loading="lazy"
+          ></iframe> */}
+          <img className="contactimg" src={contactimg}></img>
+
+          <form action="">
+            <h3>get in touch</h3>
+            <div class="inputBox">
+              <span class="fas fa-user"></span>
+              <input type="text" placeholder="name" />
+            </div>
+            <div class="inputBox">
+              <span class="fas fa-envelope"></span>
+              <input type="email" placeholder="email" />
+            </div>
+            <div class="inputBox">
+              <span class="fas fa-phone"></span>
+              <input type="number" placeholder="number" />
+            </div>
+            <input type="submit" value="contact now" class="btn" />
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Contact;
